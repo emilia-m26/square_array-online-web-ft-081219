b57dfc32134378da9b5e2fbd72b9bd0ff1bfefd2
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-25 21:06:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,7 +7,10 @@
 # end
 
 def square_array(array)
-  new_array=[]
-array.collect {|arr| new_array<< arr**2}
-return new_array
+  new_array =[]
+  array.each do |arr|
+    new_array << arr**2
+  end
+  return new_array
 end
+
